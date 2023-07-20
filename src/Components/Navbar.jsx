657669ae9ca58e99e9abc89f2497914c17261649
Anushka Rastogi <97504525+anushkarastogi@users.noreply.github.com>
2023-07-20 16:26:14 +0530

    Add files via upload

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,43 @@
+import "./Navbar.css";
+import { Link } from 'react-router-dom'
+import logo from "./practo.svg"
+export const Navigation = () => {
+    return <div className="navbar-area">
+        <nav className="nav-container">
+            <div className="div-nav-left">
+                <Link to="/"><img src={logo} alt="practo-logo" className='nav-logo' /></Link>
+            </div>
+            <div className="div-nav-middle">
+                <div className="div-nav-middle-item">
+                    <div>Find Doctors</div>
+                </div>
+
+                <div className="div-nav-middle-item">
+                    <div>Video Consult</div>
+                </div>
+                
+                    <div className="div-nav-middle-item">
+                        <div>Medicines</div>
+                    </div>
+                
+                
+                    <div className="div-nav-middle-item">
+                        <div>Lab Tests</div>
+                    </div>
+               
+                
+                <div className="div-nav-middle-item">
+                    <div>Surgeries</div>
+                </div>
+                
+                </div>
+            <div className="div-nav-right">
+                <Link to="/Auth">
+                    <button className="btn-login">
+                        Login / Signup
+                    </button>
+                </Link>
+            </div>
+        </nav>
+    </div>
+}
